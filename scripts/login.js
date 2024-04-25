e17dd53521036e3f8c50a63678b24467c6bab889
tineojf <92743762+tineodev@users.noreply.github.com>
2024-04-25 12:54:48 -0500

    Refactor login form validation and API call in login.js

--- scripts/login.js
+++ scripts/login.js
@@ -4,26 +4,28 @@ const inputPassword = document.getElementById('inputPassword');
 const formLogin = document.getElementById('formLogin');
 const spnEmail = document.getElementById('spnEmail');
 const spnPassword = document.getElementById('spnPassword');
+const urlAPI = 'http://localhost:8000/api/v1/users/login';
 
 // console.log(btnIngresar);
 // console.log(inputEmail);
 // console.log(inputPassword);
 
-formLogin.addEventListener('submit', (event) => {
+formLogin.addEventListener('submit', async (event) => {
   event.preventDefault();
 
   const email = inputEmail.value;
   const password = inputPassword.value;
 
-  console.log(email);
-  console.log(password);
+  // console.log(email);
+  // console.log(password);
 
   //todo añadir colores segun corresponda (rojo, amarillo)
   //todo usuario incorrecto
+  //todo elimine los mensajes de error al escribir en los inputs
   // valida que email & password no este vacio
   validateInput(inputEmail, spnEmail, 'email');
   validateInput(inputPassword, spnPassword, 'contraseña');
-  
+
   //valida el email con regex 
   if (validateEmail(email)) {
     console.log('Email valido');
@@ -31,8 +33,22 @@ formLogin.addEventListener('submit', (event) => {
     spnEmail.classList.add('invalid');
     spnEmail.classList.add('error');
     spnEmail.textContent = 'Email invalido';
+    return;
   }
 
+  // validar usuario existente
+  const body = {
+    email: email,
+    password: password
+  };
+
+  const data = await hacerLogin(urlAPI, null, 'POST', body);
+  console.log(data);
+
+  //todo guardar el id en localstorage
+  if (data.ok){
+    window.location.href = '/mis-tareas.html';
+  }
 });
 
 function validateInput(elemento, contenedor, texto) {
@@ -49,3 +65,21 @@ function validateEmail(email) {
   return regex.test(email);
 }
 
+async function hacerLogin(_url, _token = null, _method = 'GET', _body = null) {
+
+  const options = {
+    method: _method,
+    headers: { "Content-Type": "application/json"},
+  };
+
+  if (_token) { options.headers["Authorization"] = "Bearer " + _token; }
+  if (_body) { options.body = JSON.stringify(_body); }
+
+  try {
+    const response = await fetch(_url, options);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
